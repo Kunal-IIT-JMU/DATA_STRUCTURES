eb5346a2180746744b5ba5e2bce82b0d3a514f77
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-03-17 09:57:12 +0000

    ok

--- Doubly Linked Lists/Reversing.cpp
+++ Doubly Linked Lists/Reversing.cpp
@@ -3,7 +3,66 @@
 
 using namespace std;
 
-int main(){
+//class for creating node 
+class node {
+    public:
+    int data;
+    node* next;
+    node* prev;
 
+    node(int d){
+        data = d;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+//inserting an element in a row
+void insert_node_head(node* &head,int data){
+    node* nn = new node(data);
+    if(head == NULL){
+        head = nn;
+        return;
+    }
+    nn -> next = head;
+    head -> prev = nn;
+    head = nn;
+}
+
+//printing the Linked Lists
+void print(node* &head){
+    node* temp = head;
+    while(temp != NULL){
+        cout << temp -> data << " <~> " ;
+        temp = temp -> next;
+    }
+    cout << "NULL" << endl << endl;
+}
+
+//Function to reverse the Given Dooubly Linked Lists
+node* Reverse(node* head){
+    if(!head)
+    return NULL;
+
+    node* temp = head -> next;
+    head -> next = head -> prev;
+    head -> prev = temp;
+
+    if(!head -> prev)
+    return head;
+    else
+    return(Reverse(head->prev));
+
+}
+int main(){
+    node* head = NULL;
+    for(int i = 10; i>= 1; i--){
+        insert_node_head(head,i);
+    }
+    print(head);
+
+    //reversing the given Doubly Linked List
+    head = Reverse(head);
+    print(head);
     return 0;
 }
