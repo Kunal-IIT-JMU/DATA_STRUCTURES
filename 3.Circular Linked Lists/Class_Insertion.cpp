c07b8138a5655eb176fdc62a04fbb568b1a5ec7a
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-03-17 13:21:45 +0000

    ok

---
+++ 3.Circular Linked Lists/Class_Insertion.cpp
@@ -0,0 +1,46 @@
+//Kunal Sharma IIT JAMMU
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+void insert_head(node* &head, int data){
+    node* nn = new node(data);
+    if(head == NULL){
+        nn -> next = nn;
+        head = nn;
+        return ;
+    }
+    node* temp = head;
+    while(temp -> next != head){
+        temp = temp -> next;
+    }
+    temp -> next = nn;
+    nn -> next = head;
+    head = nn;
+}
+
+void insert_tail(node* &head, int data){
+    node* temp = head;
+    node* nn = new node(data);
+    while(temp -> next != head){
+        temp = temp -> next;
+    }
+    temp -> next = nn;
+    nn -> next = head;
+}
+
+int main(){
+
+    return 0;
+}
