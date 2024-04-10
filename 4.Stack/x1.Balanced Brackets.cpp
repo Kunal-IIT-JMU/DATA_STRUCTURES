f12f3584f00bc0a5de413582ac9f7d0034a7c65a
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-04-10 07:30:42 +0000

    ok

--- 4.Stack/x1.Balanced Brackets.cpp
+++ 4.Stack/x1.Balanced Brackets.cpp
@@ -8,7 +8,33 @@ int main(){
     string elements;
     cin >> elements;
     for(int i =0; i<elements.length(); i++){
-        
+        if(elements[i] == '(' || elements[i] == '{' || elements[i] == '['){
+            ele.push(elements[i]);
+        }
+        else{
+            if(!ele.empty()){
+                char t = ele.top();
+                if(elements[i] == '}' && t == '{'  || elements[i] == ')' && t == '(' || elements[i] == ']' && t == '['){
+                    ele.pop();
+                }
+                else{
+                    cout << "Not Balanced" << endl;
+                    return 0;
+                }
+            }
+            else{
+                cout << "Not Balanced" << endl;
+                return 0;
+            }
+        }
+    }
+    if(ele.empty()){
+        cout << "Balanced" << endl;
+        return 0;
+    }
+    else{
+        cout << "Not Balanced" << endl;
+        return 0;
     }
     return 0;
 }
