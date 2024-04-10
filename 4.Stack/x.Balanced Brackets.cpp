aaea98224a9e2389e1dcffd9210e898b1e029f59
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-04-10 07:19:53 +0000

    ok

--- 4.Stack/x.Balanced Brackets.cpp
+++
@@ -1,9 +0,0 @@
-//Kunal Sharma IIT JAMMU
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-    
-    return 0;
-}
---
+++ 4.Stack/x1.Balanced Brackets.cpp
@@ -0,0 +1,14 @@
+//Kunal Sharma IIT JAMMU
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    stack <char> ele;
+    string elements;
+    cin >> elements;
+    for(int i =0; i<elements.length(); i++){
+        
+    }
+    return 0;
+}
